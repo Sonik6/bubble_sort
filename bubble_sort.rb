a49623d144b7f8243259008732bb9daecd668003
Slawek Galka <galka.slawek6@gmail.com>
2021-12-06 19:30:05 +0100

    bubble_srt algorithm

---
+++ bubble_sort.rb
@@ -0,0 +1,26 @@
+numbers = [4, 3, 78, 2, 0, 2]
+
+# Our method will take an array as a parameter
+def bubble_sort(array)
+    array_length = array.size
+    return array if array_length <= 1
+  
+    loop do
+      # we need to create a variable that will be checked so that we don't run into an infinite loop scenario.
+      swapped = false
+  
+      # subtract one because Ruby arrays are zero-index based
+      (array_length - 1).times do |i|
+        if array[i] > array[i + 1]
+          array[i], array[i + 1] = array[i + 1], array[i]
+          swapped = true
+        end
+      end
+  
+      break unless swapped
+    end
+  
+    print array
+  end
+  
+  bubble_sort(numbers)
